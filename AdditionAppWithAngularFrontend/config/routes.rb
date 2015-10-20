d98821d5bba6c3057e880bbd61ba82c9bd29d5e6
Dan Serban <dserban01@yahoo.com>
2015-10-20 18:34:27 +0300

    Remove GET routes

--- AdditionAppWithAngularFrontend/config/routes.rb
+++ AdditionAppWithAngularFrontend/config/routes.rb
@@ -1,8 +1,5 @@
 Rails.application.routes.draw do
 
-  get '/showinputfields' => 'addition#showinputfields'
-  get '/displayresult' => 'addition#displayresult'
-
   post '/performaddition' => 'addition#performaddition'
 
 end
